fd75ee632a24f2df754e84f3327e13fce6f71f94
Holinda Beshiri <holinda_b@hotmail.com>
2024-06-28 22:58:39 +0000

    Update shopping bag template and views , issue still not fixed

--- bag/views.py
+++ bag/views.py
@@ -1,84 +1,100 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 
-from products.models import Product, Color
+from products.models import Product
 
 # Create your views here.
 
 def view_bag(request):
-    """ A view that renders the bag contents page """
-
+    """A view that renders the bag contents page"""
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
-
+    """Add a quantity of the specified product to the shopping bag"""
+    
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
+    size = request.POST.get('product_size') if 'product_size' in request.POST else None
+    color = request.POST.get('product_color') if 'product_color' in request.POST else None
+
     bag = request.session.get('bag', {})
 
-    if size:
-        if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
-            else:
-                bag[item_id]['items_by_size'][size] = quantity
-        else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
+    if item_id not in bag:
+        bag[item_id] = {'items_by_size': {}}
+
+    if size and color:
+        if color not in bag[item_id]['items_by_size']:
+            bag[item_id]['items_by_size'][color] = {}
+        if size not in bag[item_id]['items_by_size'][color]:
+            bag[item_id]['items_by_size'][color][size] = 0
+        bag[item_id]['items_by_size'][color][size] += quantity
+    elif size:
+        if size not in bag[item_id]['items_by_size']:
+            bag[item_id]['items_by_size'][size] = 0
+        bag[item_id]['items_by_size'][size] += quantity
     else:
-        if item_id in list(bag.keys()):
-            bag[item_id] += quantity
-        else:
-            bag[item_id] = quantity
+        if 'quantity' not in bag[item_id]:
+            bag[item_id]['quantity'] = 0
+        bag[item_id]['quantity'] += quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-    
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
-
+    
     quantity = int(request.POST.get('quantity'))
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
+    size = request.POST.get('product_size') if 'product_size' in request.POST else None
+    color = request.POST.get('product_color') if 'product_color' in request.POST else None
+
     bag = request.session.get('bag', {})
 
-    if size:
+    if size and color:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][color][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][color][size]
+            if not bag[item_id]['items_by_size'][color]:
+                del bag[item_id]['items_by_size'][color]
+            if not bag[item_id]['items_by_size']:
+                del bag[item_id]
+    elif size:
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
+                del bag[item_id]
     else:
         if quantity > 0:
-            bag[item_id] = quantity
+            bag[item_id]['quantity'] = quantity
         else:
-            bag.pop(item_id)
+            del bag[item_id]
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
-
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
     try:
-        size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
+        size = request.POST.get('product_size') if 'product_size' in request.POST else None
+        color = request.POST.get('product_color') if 'product_color' in request.POST else None
+
         bag = request.session.get('bag', {})
 
-        if size:
+        if size and color:
+            del bag[item_id]['items_by_size'][color][size]
+            if not bag[item_id]['items_by_size'][color]:
+                del bag[item_id]['items_by_size'][color]
+            if not bag[item_id]['items_by_size']:
+                del bag[item_id]
+        elif size:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
+                del bag[item_id]
         else:
-            bag.pop(item_id)
+            del bag[item_id]
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
