9fc8f8cb65d8ed667b133c1794ac58932530112f
Holinda Beshiri <holinda_b@hotmail.com>
2024-06-29 19:14:10 +0000

    Add randomized top rated products to index view and index.html

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from products.models import Product
 
-# Create your views here.
 def index(request):
-    """ A view to return the index page """
+    """A view to return the index page with top-rated products"""
 
-    return render(request, 'home/index.html')
+    # Fetch top-rated products ordered by rating (highest first)
+    top_rated_products = Product.objects.order_by('-rating')[:4]
+
+    # Render the template with top-rated products
+    return render(request, 'home/index.html', {'randomised': top_rated_products})
