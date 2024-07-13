4fb0e6702b8b9674e2a2af512e0f33370016ee45
Holinda Beshiri <holinda_b@hotmail.com>
2024-07-13 13:58:28 +0000

    Product Admin- Add prodct view, url and template

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Category, Color
+from .forms import ProductForm
 
 
 # Create your views here.
@@ -75,3 +76,14 @@ def list_colors(request):
     colors = Color.objects.all()
     
     return render(request, 'product_detail.html', {'colors': colors})
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
