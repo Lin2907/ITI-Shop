13ad877e50a6557a722396635e947f28764dfa7f
Holinda Beshiri <holinda_b@hotmail.com>
2024-07-28 20:51:53 +0000

    Add the review model to admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category , Color
+from .models import Product, Category , Color,Review
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
@@ -25,3 +25,4 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Color)
+admin.site.register(Review)
