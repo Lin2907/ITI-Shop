9c39c88079d3c8cf1a1432618dff71570377a9bf
Holinda Beshiri <holinda_b@hotmail.com>
2024-09-09 18:47:47 +0000

    Update wishlist and profiles py files to pass PEP8 validation

--- wishlist/models.py
+++ wishlist/models.py
@@ -4,9 +4,13 @@ from products.models import Product
 
 # Create your models here.
 
+
 class Wishlist(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='wishlist')
-    products = models.ManyToManyField(Product, related_name='wishlisted_by', blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name="wishlist")
+    products = models.ManyToManyField(Product,
+                                      related_name="wishlisted_by", blank=True)
 
     def __str__(self):
         return f"{self.user.username}'s Wishlist"
+        
