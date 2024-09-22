0b015069877a6724ad7293d2ff4f6125f197efb2
Holinda Beshiri <holinda_b@hotmail.com>
2024-09-22 11:24:38 +0000

    Update wishlist option on product detail page for better styling and fix small long line errors in wishlist model.py

--- wishlist/models.py
+++ wishlist/models.py
@@ -3,9 +3,13 @@ from django.contrib.auth.models import User
 from products.models import Product
 from django.utils import timezone
 
+
 class Wishlist(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="wishlist")
-    products = models.ManyToManyField(Product, related_name='wishlisted_by', blank=True)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name="wishlist")
+    products = models.ManyToManyField(Product, related_name='wishlisted_by',
+                                      blank=True)
     date_added = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
