3205881a452114d8d4c6dda5c49bc895dc38f33d
Holinda Beshiri <holinda_b@hotmail.com>
2024-10-10 19:45:37 +0000

    Fix auth error by changing the smtp server to gmail instead of outlook and registred admin for wishlist and contact

--- iti_shop/settings.py
+++ iti_shop/settings.py
@@ -231,7 +231,10 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp-mail.outlook.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST = 'smtp.gmail.com'
+    #EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    #EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    #DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_USER = 'holbesh2013@gmail.com'
+    EMAIL_HOST_PASSWORD = 'xtvgawgpvplfhfif'
+    DEFAULT_FROM_EMAIL = 'holbesh2013@gmail.com'
